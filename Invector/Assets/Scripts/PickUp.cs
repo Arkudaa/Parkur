a75c1c4f9fcd76538bebb10fe5781771b032668a
Arkudaa <71982604+Arkudaa@users.noreply.github.com>
2022-10-12 16:02:51 +0300

    vers

---
+++ Invector/Assets/Scripts/PickUp.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickUp : MonoBehaviour
+{
+    public AudioSource coinmusic;
+    public int score;
+    public Text cointext;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //cointext.text = "Score: " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "coin")
+        {
+            Destroy(collision.gameObject);
+            score++;
+           coinmusic.Play();
+           cointext.text = "Score: " + score;
+        }
+
+        if (collision.gameObject.tag == "danger")
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+        
+    }
+}
---
+++ Invector/Assets/nebo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nebo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("test"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * 5);
+    }
+}
